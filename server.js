00516007eb543bf3398d2494860e710de623011e
faballen <freezamillerstella@gmail.com>
2020-09-26 19:09:38 -0500

    adding scripts modification on the Server.js file

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const request = require('request');
 let bodyParser = require('body-parser');
-const { callbackify } = require('util');
 const app = express();
 app.use(express.json());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname));
 const port = 8080;
 
@@ -20,25 +20,18 @@ let options = {
     }
   };
 
-// let fetchFromAPI = callback => {
-//     request(options, (error, response, body) => {
-//         if (error) throw err
-//         console.log('getting data......');
-//         console.log(body);
-//         return callback(err, JSON.parse(body));
-//     });
-// }
-
-/** restFul api */
-app.get("/api/allPlayers", (req, res) => {
-
     request(options, (error, response, body) => {
         if (error) throw err
-        console.log('getting data......');
-        console.log(JSON.parse(body));
-        res.send(data);
+        let jsonData = JSON.parse(body);
+        console.log(jsonData);
+    
     });
-})
+
+
+/** restFul api */
+// app.get("/api/allPlayers", (req, res) => {
+
+// });
 
 app.listen(port, () => {
     console.log(`I am listening to a port ${port}`);
